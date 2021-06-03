5d3b6ab39ae4ddfe1fd15190ab608d43263be8f5
bholties <77474724+bholties@users.noreply.github.com>
2021-06-02 21:31:53 -0500

    SVG wrapper

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,16 @@
-// @TODO: YOUR CODE HERE!
+var svgWidth = 960;
+var svgHeight = 500;
+var margin = {top:20, right:40, bottom: 60, left: 100};
+var width = svgWidth - margin.left - margin.right;
+var height = svgHeight - margin.top - margin.bottom;
+
+var svg = d3
+    .select(".chart")
+    .append("svg")
+    .attr("width",svgWidth)
+    .attr("height",svgHeight)
+    .append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+var chart = svg.append("g");
+
+d3.select(".chart").append("div").attr("class","tooltip").style("opacity",0);
