40964788c005dd030e9651f8c44edb9b9ddeb4ca
bholties <77474724+bholties@users.noreply.github.com>
2021-06-02 21:55:24 -0500

    import data

--- assets/js/app.js
+++ assets/js/app.js
@@ -4,6 +4,7 @@ var margin = {top:20, right:40, bottom: 60, left: 100};
 var width = svgWidth - margin.left - margin.right;
 var height = svgHeight - margin.top - margin.bottom;
 
+Create SVG wrapper
 var svg = d3
     .select(".chart")
     .append("svg")
@@ -14,3 +15,10 @@ var svg = d3
 var chart = svg.append("g");
 
 d3.select(".chart").append("div").attr("class","tooltip").style("opacity",0);
+
+Import Data
+d3.csv("data.csv", function(err, healthData) {
+    if (err) throw err;
+  console.log(healthData)
+
+  
